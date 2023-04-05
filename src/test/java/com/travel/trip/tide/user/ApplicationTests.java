cb897283590facde9ae49822f297244bd8c115dc
Dmytro Bekker <dimabekker863@gmail.com>
2023-04-05 16:20:46 +0300

    Simplest test for sonar qube check

--- src/test/java/com/travel/trip/tide/user/ApplicationTests.java
+++ src/test/java/com/travel/trip/tide/user/ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.travel.trip.tide.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(context);
 	}
 
 }
