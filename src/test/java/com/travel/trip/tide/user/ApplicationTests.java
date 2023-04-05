3fb705067b0c5abb37e7ec28b54e9006fc1758b5
Dmytro Bekker <dimabekker863@gmail.com>
2023-04-05 14:50:04 +0300

    Initial project set up

---
+++ src/test/java/com/travel/trip/tide/user/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.trip.tide.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
